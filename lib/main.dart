0c7c93601b50f9338a7922021dd344b10fee8daa
Ecommerce Demo <mohammadsaidi1995@gmail.com>
2023-11-27 21:25:10 +0600

    Material app title changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Done by Mohammad Saidi',
       theme: ThemeData(
         // This is the theme of your application.
         //
